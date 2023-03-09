32742b22e51cbbb6f1c9775ca64611986fd9e359
meita33 <124659672+meita33@users.noreply.github.com>
2023-03-09 18:28:45 +0900

    商品出品画面へ遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   # has_one :order
   has_one_attached :image
   belongs_to_active_hash :prefecture
 
 
-  validates :
-  validates :
-  validates :
+  # validates :
+  # validates :
+  # validates :
 
   validates :category_id
   validates :quality_id
