49c6f1a2755b1c160ff6a62615afada3819c76e1
meita33 <124659672+meita33@users.noreply.github.com>
2023-03-10 18:36:21 +0900

    販売手数料や販売利益の計算

--- app/models/item_price.js
+++ app/models/item_price.js
@@ -1,2 +1,11 @@
-const priceInput = document.getElementById("item-price");
-console.log(priceInput);
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue * 0.9)
+  })
+});
