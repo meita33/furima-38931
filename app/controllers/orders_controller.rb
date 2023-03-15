a7d53469f4139e77141a5c031dc21b319d83983f
meita33 <124659672+meita33@users.noreply.github.com>
2023-03-15 21:30:26 +0900

    購入機能の動作の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,19 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @order = Order.new
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
     redirect_to root_path if current_user == @item.user
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+     redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
@@ -21,6 +22,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:order_id, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, )
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
